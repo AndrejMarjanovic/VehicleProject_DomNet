95f8598e6022d347dee033cf2d8d2c067a7eabe2
AndrejMarjanovic <andrej.marjanovic@edu.fit.ba>
2023-02-01 21:35:20 +0100

    Model.Common i Entiteti u DAL

--- Vehicle_DomNet/Vehicle.DAL/Entiteti/VehicleModel.cs
+++ Vehicle_DomNet/Vehicle.DAL/Entiteti/VehicleModel.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Vehicle.Model.Common;
 
-namespace Vehicle.Model
+namespace Vehicle.DAL.Entiteti
 {
-    public class VehicleMake : IVehicleMake
+    public class VehicleModel
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Abrv { get; set; } = null!;
+        public int VehicleMakeId { get; set; }
+        public virtual VehicleMake VehicleMake { get; set; } = null!;
     }
 }
--- Vehicle_DomNet/Vehicle.Model/VehicleModel.cs
+++ Vehicle_DomNet/Vehicle.Model/VehicleModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vehicle.Model.Common;
 
 namespace Vehicle.Model
 {
-    public class VehicleModel
+    public class VehicleModel : IVehicleModel
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
