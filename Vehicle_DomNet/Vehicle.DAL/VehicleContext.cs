a5e1de22e427eee06ce2c34e55c5ab91e37f4cbc
AndrejMarjanovic <andrej.marjanovic@edu.fit.ba>
2023-02-01 11:55:58 +0100

    Dodani modeli, db context i api

---
+++ Vehicle_DomNet/Vehicle.DAL/VehicleContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicle.Model;
+
+namespace Vehicle.DAL
+{
+    public partial class VehicleContext : DbContext
+    {
+        public VehicleContext(DbContextOptions<VehicleContext> options) : base(options) { }
+
+        public VehicleContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {         
+
+        }
+
+        public DbSet<VehicleMake> VehicleMake { get; set; }
+        public DbSet<VehicleModel> VehicleModel { get; set; }
+    }
+}
